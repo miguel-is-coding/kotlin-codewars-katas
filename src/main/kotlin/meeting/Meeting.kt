dc3a7d7abc675ce6dece2e896a7e775cd30636da
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-02-16 13:08:29 +0000

    ref: rename variables.

--- src/main/kotlin/meeting/Meeting.kt
+++ src/main/kotlin/meeting/Meeting.kt
@@ -3,14 +3,14 @@ package org.example.meeting
 class Meeting(private val members: String) {
     fun orderMembers(): String {
         if (members.isEmpty()) return "()"
-        val membersList = members
+        val fullNamesOrdered = members
             .split(";")
             .sortedBy { it.getOrNull(it.indexOf(":")+1) }
         var membersOrdered = ""
-        membersList.forEach {
-            val memberFullName = it.split(":")
-            val lastname = memberFullName[1].uppercase()
-            val firstname = memberFullName[0].uppercase()
+        fullNamesOrdered.forEach {
+            val fullName = it.split(":")
+            val lastname = fullName[1].uppercase()
+            val firstname = fullName[0].uppercase()
             membersOrdered += "($lastname, $firstname)"
         }
         return membersOrdered
