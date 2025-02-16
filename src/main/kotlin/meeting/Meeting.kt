688189bd543863306c87916dc592340ebe0f78b6
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-02-16 14:23:02 +0000

    ref: use functional solution to parse, sort and format list of members.

--- src/main/kotlin/meeting/Meeting.kt
+++ src/main/kotlin/meeting/Meeting.kt
@@ -3,18 +3,23 @@ package org.example.meeting
 class Meeting(private val members: String) {
     fun orderMembers(): String {
         if (members.isEmpty()) return "()"
-        val fullNamesOrdered = members.split(";")
-            .sortedWith(compareBy<String> {
-                val lastNameInitial = it.getOrNull(it.indexOf(":") + 1)
-                lastNameInitial
-            } .thenBy {
-                val firstNameInitial = it.first()
-                firstNameInitial
-            })
-        return fullNamesOrdered.joinToString("") {
-            it.split(":").let { (firstname, lastname) ->
-                "(${lastname.uppercase()}, ${firstname.uppercase()})"
-            }
-        }
+        return members.split(";")
+            .map { parseFrom(it) }
+            .let { sortByNameAndLastname(it) }
+            .joinToString("") { formatFullNameFrom(it) }
+    }
+
+    private fun parseFrom(member: String): Pair<String, String> {
+        val (firstname, lastname) = member.split(":")
+        return firstname to lastname
+    }
+
+    private fun sortByNameAndLastname(members: List<Pair<String, String>>): List<Pair<String, String>> {
+        return members.sortedWith(compareBy({ it.second }, { it.first }))
+    }
+
+    private fun formatFullNameFrom(member: Pair<String, String>): String {
+        val (firstname, lastname) = member
+        return "(${lastname}, ${firstname})".uppercase()
     }
 }
