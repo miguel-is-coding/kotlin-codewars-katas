2b75bcecef81796d551acf2a6dd952b7f79281fb
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-02-16 13:25:18 +0000

    green: order meeting members by firstname when lastname is equal.

--- src/main/kotlin/meeting/Meeting.kt
+++ src/main/kotlin/meeting/Meeting.kt
@@ -3,9 +3,14 @@ package org.example.meeting
 class Meeting(private val members: String) {
     fun orderMembers(): String {
         if (members.isEmpty()) return "()"
-        val fullNamesOrdered = members
-            .split(";")
-            .sortedBy { it.getOrNull(it.indexOf(":")+1) }
+        val fullNamesOrdered = members.split(";")
+            .sortedWith(compareBy<String> {
+                val lastNameInitial = it.getOrNull(it.indexOf(":") + 1)
+                lastNameInitial
+            } .thenBy {
+                val firstNameInitial = it.first()
+                firstNameInitial
+            })
         var membersOrdered = ""
         fullNamesOrdered.forEach {
             val fullName = it.split(":")
