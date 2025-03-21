d910428706f0c2bd9450cc787ad8e3137a5742dd
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-03-21 12:32:11 +0000

    green: make rock lose against paper.

--- src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
+++ src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
@@ -2,7 +2,8 @@ package org.example.rockPaperScissors
 
 object RockPaperScissors{
     fun play(player1: Player, player2: Player): String {
-        if (player1.choice == Choices.ROCK || player1.choice == Choices.PAPER) {
+        if ((player1.choice == Choices.ROCK && player2.choice == Choices.SCISSORS)
+            || (player1.choice == Choices.PAPER && player2.choice == Choices.ROCK)) {
             return "Player 1 wins"
         }
         return "Player 2 wins"
