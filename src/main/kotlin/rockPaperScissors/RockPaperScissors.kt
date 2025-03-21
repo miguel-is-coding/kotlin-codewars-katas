3df6f29e757884b6778736d995675884dac96465
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-03-21 12:10:37 +0000

    ref: create choices enum.

--- src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
+++ src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
@@ -2,11 +2,16 @@ package org.example.rockPaperScissors
 
 object RockPaperScissors{
     fun play(player1: Player, player2: Player): String {
-        if (player1.choice == "rock") {
+        if (player1.choice == Choices.ROCK) {
             return "Player 1 wins"
         }
         return "Player 2 wins"
     }
 }
 
-data class Player(val choice: String)
+data class Player(val choice: Choices)
+
+enum class Choices {
+    ROCK,
+    SCISSORS
+}
--- src/test/kotlin/rockPaperScissors/RockPaperScissorsShould.kt
+++ src/test/kotlin/rockPaperScissors/RockPaperScissorsShould.kt
@@ -2,16 +2,17 @@ package rockPaperScissors
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import org.example.rockPaperScissors.Choices
 import org.example.rockPaperScissors.Player
 import org.example.rockPaperScissors.RockPaperScissors
 
 class RockPaperScissorsShould: FunSpec({
     test("make rock beat scissors") {
-        RockPaperScissors.play(Player("rock"), Player("scissors")) shouldBe "Player 1 wins"
+        RockPaperScissors.play(Player(Choices.ROCK), Player(Choices.SCISSORS)) shouldBe "Player 1 wins"
     }
 
     test("make scissors lose against rock") {
-        RockPaperScissors.play(Player("scissors"), Player("rock")) shouldBe "Player 2 wins"
+        RockPaperScissors.play(Player(Choices.SCISSORS), Player(Choices.ROCK)) shouldBe "Player 2 wins"
     }
 })
 
