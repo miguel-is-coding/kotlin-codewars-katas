f8e4dab3ac2ff627a4083e76a3e0020d0a75d715
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-03-21 12:40:57 +0000

    green: make scissors beat paper.

--- src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
+++ src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
@@ -3,11 +3,19 @@ package org.example.rockPaperScissors
 object RockPaperScissors{
     fun play(player1: Player, player2: Player): String {
         if ((player1.choice == Choices.ROCK && player2.choice == Choices.SCISSORS)
-            || (player1.choice == Choices.PAPER && player2.choice == Choices.ROCK)) {
+            || (player1.choice == Choices.PAPER && player2.choice == Choices.ROCK)
+            || (player1.choice == Choices.SCISSORS && player2.choice == Choices.PAPER)){
             return "Player 1 wins"
         }
         return "Player 2 wins"
     }
+
+
+    /**
+     * rock beats scissors
+     * paper beats rock
+     * scissors beat paper
+     */
 }
 
 data class Player(val choice: Choices)
