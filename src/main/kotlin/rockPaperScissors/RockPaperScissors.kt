13143bbbd345d0a5e490f3c038f7d13678a9c1af
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-03-21 13:00:41 +0000

    ref: extract comparisons to explanatory method.

--- src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
+++ src/main/kotlin/rockPaperScissors/RockPaperScissors.kt
@@ -1,21 +1,24 @@
 package org.example.rockPaperScissors
 
-object RockPaperScissors{
+object RockPaperScissors {
     fun play(player1: Player, player2: Player): String {
-        if ((player1.choice == Choices.ROCK && player2.choice == Choices.SCISSORS)
-            || (player1.choice == Choices.PAPER && player2.choice == Choices.ROCK)
-            || (player1.choice == Choices.SCISSORS && player2.choice == Choices.PAPER)){
-            return "Player 1 wins"
+        return if (wins(player1, player2)
+        ) {
+            "Player 1 wins"
+        } else {
+            "Player 2 wins"
         }
-        return "Player 2 wins"
     }
 
-
-    /**
-     * rock beats scissors
-     * paper beats rock
-     * scissors beat paper
-     */
+    private fun wins(
+        player1: Player,
+        player2: Player
+    ): Boolean {
+        val rockBeatsScissors = player1.choice == Choices.ROCK && player2.choice == Choices.SCISSORS
+        val paperBeatsRock = player1.choice == Choices.PAPER && player2.choice == Choices.ROCK
+        val scissorsBeatsPaper = player1.choice == Choices.SCISSORS && player2.choice == Choices.PAPER
+        return (rockBeatsScissors || paperBeatsRock || scissorsBeatsPaper)
+    }
 }
 
 data class Player(val choice: Choices)
