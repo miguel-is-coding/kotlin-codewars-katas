3b7afa524feee919894932ff07095426908fd4fc
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2025-03-21 11:54:26 +0000

    red: make rock beat scissors.

---
+++ src/main/kotlin/rockPaperScissors/README.md
@@ -0,0 +1,21 @@
+# Rock paper Scissors kata
+
+The classic rock-paper-scissors game
+
+## Case scenarios
+```
+As a player
+I want rock to beat scissors
+```
+
+## Solution
+```
+Given I have chosen rock
+When the opponent chooses scissors
+Then I should win
+```
+```
+Given I have chosen scissors
+When the opponent chooses rock
+Then the opponent win
+```
---
+++ src/test/kotlin/rockPaperScissors/RockPaperScissorsShould.kt
@@ -0,0 +1,16 @@
+package rockPaperScissors
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class RockPaperScissorsShould: FunSpec({
+    test("make rock beat scissors") {
+        RockPaperScissors.play("rock", "scissors") shouldBe "Player 1 wins"
+    }
+})
+
+object RockPaperScissors{
+    fun play(player1Choice: String, player2Choice: String): String {
+        return TODO("Provide the return value")
+    }
+}
